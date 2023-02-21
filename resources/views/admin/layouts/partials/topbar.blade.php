c99ec3118286d003609a550476455a0fdde96f77
thepermonica <99553574+thepermonica@users.noreply.github.com>
2023-02-21 11:36:38 +0330

    Change Admin Panel Color

--- resources/views/admin/layouts/partials/topbar.blade.php
+++ resources/views/admin/layouts/partials/topbar.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+<nav class="navbar navbar-expand navbar-light adminTopbarColor topbar mb-4 static-top shadow">
 
     <!-- Sidebar Toggle (Topbar) -->
-    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle">
+    <button id="sidebarToggleTop" class="btn btn-light d-md-none rounded-circle">
         <i class="fa fa-bars" style="margin-top: 4px;"></i>
     </button>
 
@@ -14,9 +14,9 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                aria-haspopup="true" aria-expanded="false">
-                <span class="ml-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->firstname}}</span>
+                <span class="ml-2 d-none d-lg-inline text-white small">{{auth()->user()->firstname}}</span>
                 {{--                <img class="img-profile rounded-circle" src="{{ asset('/images/user.jpg') }}">--}}
-                <i class="fas fa-user fa-sm fa-fw ml-2 text-gray-400"></i>
+                <i class="fas fa-user fa-sm fa-fw ml-2 text-gray-100"></i>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in text-right"
